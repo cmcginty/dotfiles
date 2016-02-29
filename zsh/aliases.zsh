dd5243c35deb9349438e3c934fa3cf4df8e5dd07
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-02-29 00:27:26 -1000

    Add aptrm-linux alias to remove old kernels

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,6 +6,13 @@ alias aptrm-conf='aptitude -F %p search "~c" | xargs sudo dpkg -P'
 alias aptrm='apt-get purge'
 alias aptug='apt-get upgrade'
 alias aptup='apt-get update'
+function aptrm-linux() {
+   sudo apt-get purge $(for tag in "linux-image" "linux-headers" "linux-image-extra"
+                        do dpkg-query -W -f'${Package}\n' "$tag-[0-9]*.[0-9]*.[0-9]*" |
+                           sort -V |
+                           awk 'index($0,c){exit} //' c=$(uname -r | cut -d- -f1,2)
+                        done)
+}
 
 # tweet a string of text
 function t() { echo "$@" | bti }
