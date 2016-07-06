982b4532011604dbcb520c062986da390275a8ed
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-07-06 00:36:03 -1000

    Update term color file; remove mouse.zsh (use zsh.after/)

--- terminal/colors.zsh
+++ terminal/colors.zsh
@@ -1,3 +1,3 @@
 # set the default terminal color theme
 
-source $HOME/.dotfiles/terminal/base16-shell/base16-default.dark.sh
+source $HOME/.dotfiles/terminal/base16-shell/scripts/base16-default-dark.sh
