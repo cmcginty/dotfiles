8df094433a11597837378b6f8b8ae5d2ae055e84
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-09-24 17:20:19 -1000

    Update virtualenv command details

--- python/python.zsh
+++ python/python.zsh
@@ -1,8 +1,23 @@
 # Enable virtualenvwrapper (https://virtualenvwrapper.readthedocs.io/en/latest/)
 # Adds the following commands:
 #
-#     workon
-#     lssitepackages
-#     mkvirtualenv NAME
+#   add2virtualenv:        add directory to the import path
+#   allvirtualenv:         run a command in all virtualenvs
+#   cdproject:             change directory to the active project
+#   cdsitepackages:        change to the site-packages directory
+#   cdvirtualenv:          change to the $VIRTUAL_ENV directory
+#   cpvirtualenv:          duplicate the named virtualenv to make a new one
+#   lssitepackages:        list contents of the site-packages directory
+#   lsvirtualenv:          list virtualenvs
+#   mkproject:             create a new project directory and its associated virtualenv
+#   mktmpenv:              create a temporary virtualenv
+#   mkvirtualenv:          Create a new virtualenv in $WORKON_HOME
+#   rmvirtualenv:          Remove a virtualenv
+#   setvirtualenvproject:  associate a project directory with a virtualenv
+#   showvirtualenv:        show details of a single virtualenv
+#   toggleglobalsitepackages: turn access to global site-packages on/off
+#   virtualenvwrapper:     show this help message
+#   wipeenv:               remove all packages installed in the current virtualenv
+#   workon:                list or change working virtualenvs
 #
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
