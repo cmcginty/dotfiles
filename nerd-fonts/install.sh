cdf2f9265bbf83a0c6ef8802536b4c978f742125
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-09-28 14:16:23 -1000

    Switch powerline font to Ubuntu Mono to match Vimrc

--- nerd-fonts/install.sh
+++ nerd-fonts/install.sh
@@ -5,11 +5,12 @@ IFS=$'\n\t'
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
-REPO="https://github.com/ryanoasis/nerd-fonts/blob/0.6.0/patched-fonts"
-URL="$REPO/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.ttf"
+REPO="https://github.com/ryanoasis/nerd-fonts/blob/0.8.0/patched-fonts"
+URL="$REPO/UbuntuMono/Regular/complete/Ubuntu%20Mono%20derivative%20Powerline%20Nerd%20Font%20Complete.ttf"
 
 FILE=$(sed 's/%20/ /g' <<<${URL##*/})
 
 if [[ ! -e "$FILE" ]]; then
    curl -fLo "$FILE" "${URL}?raw=true"
+   sudo fc-cache -f -v
 fi
