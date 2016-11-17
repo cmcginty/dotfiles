b8950342b117399b1ddedfe806233b984d83f92b
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-11-17 10:32:02 -0800

    Update OSX terminal key settings; Activate term when Vim exists

--- terminal/config.zsh
+++ terminal/config.zsh
@@ -3,3 +3,9 @@ stty -ixon
 
 # disable Ctrl-L (clear_screen) to prevent accidental usage
 bindkey '\C-l' undefined-key
+
+if [[ "$OSTYPE" == darwin* ]]; then
+   # increase key repeat rate
+   defaults write -g InitialKeyRepeat -int 14   # normal minimum is 15 (225 ms)
+   defaults write -g KeyRepeat -int 2           # normal minimum is 2 (30 ms)
+fi
--- terminal/install.sh
+++ terminal/install.sh
@@ -3,10 +3,6 @@ set -eou pipefail
 IFS=$'\n\t'
 
 if [[ "$OSTYPE" == darwin* ]]; then
-   # increase key repeat rate
-   defaults write -g InitialKeyRepeat -int 10   # normal minimum is 15 (225 ms)
-   defaults write -g KeyRepeat -int 1           # normal minimum is 2 (30 ms)
-
    # add command line tools
    xcode-select -p >/dev/null || xcode-select --install
 
