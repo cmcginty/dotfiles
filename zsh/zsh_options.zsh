0fd5b1f777e74d8579a482b5b5c6f89ee13db1e1
Casey McGinty <casey.mcginty@gmail.com>
2016-12-19 16:47:21 -1000

    vim bindings in ZSH

--- zsh/zsh_options.zsh
+++ zsh/zsh_options.zsh
@@ -1,20 +1,30 @@
-# why would you type 'cd dir' if you could just type 'dir'?
+# 'cd' to dir without typing the 'cd' command
 setopt AUTO_CD
-# this makes cd=pushd
+# Makes cd=pushd, 'popd' to go back
 setopt AUTO_PUSHD
-# this will ignore multiple directories for the stack.  Useful?  I dunno.
+# Ignore multiple directories for the stack.
 setopt PUSHD_IGNORE_DUPS
 # 10 second wait if you do something that will delete everything.
 setopt RM_STAR_WAIT
-# case insensitive globbing
+# Case insensitive globbing
 setopt NO_CASE_GLOB
-# be reasonable!
+#
 setopt NUMERIC_GLOB_SORT
-# I don't know why I never set this before.
+#
 setopt EXTENDED_GLOB
-# oh wow!  This is killer...  try it!
+#
 bindkey -M vicmd 'q' push-line
 # !NNN, history auto-completion after space
 bindkey -M viins ' ' magic-space
-# catch accidental CTRL-D
-# setopt ignoreeof
+
+# vim command mode with <CTRL><SPACE>
+bindkey '^ ' vi-cmd-mode
+
+# common vim bindings
+bindkey -a 'gg' beginning-of-buffer-or-history
+bindkey -a 'g~' vi-oper-swap-case
+bindkey -a G end-of-buffer-or-history
+
+# vim backspace doesn't stop where you started insert mode
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
