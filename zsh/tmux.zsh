013e07d4dfc68d3203ca65b21196312a6a302c9b
Casey McGinty <casey.mcginty@gmail.com>
2017-04-13 10:30:44 -0700

    Manually set tmux window name during SSH
    I use a lot of remote hosts that set the window title to a very long
    value. This lets me have more control over window names in tmux.

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,3 +1,11 @@
 # Reset the term name after SSH exits.
 # TODO: Update this to set windows as a short directory name.
 add-zsh-hook precmd f(){tmux rename-window "zsh"}
+
+# Wrap SSH command so that window gets renamed correctly
+ssh() {
+   OLD_NAME=$(tmux display-message -p '#W')
+   tmux rename-window "${@[-1]}"    # rename window using last arg
+   command ssh $@
+   tmux rename-window "$OLD_NAME"   # restore previous name
+}
