559005a63e10b5bcfdaae3161586fa7768076b18
Casey McGinty <casey.mcginty@gmail.com>
2020-01-11 12:12:02 -0800

    Add youtube aliases

--- zsh/aliases-media.zsh
+++ zsh/aliases-media.zsh
@@ -14,3 +14,8 @@ alias cdrdao-flac='flac -f -d *.flac && mktoc *.cue > out.toc && cdrdao-write ou
 alias cdrdao-write='sudo cdrdao write --eject --device /dev/sr0'
 alias cdrdao='sudo cdrdao'
 alias mktoc='mktoc -c -18'
+
+# Youtube
+alias ytdl='youtube-dl -i --cookies ~/cookies.txt --format mp4 --mark-watched --output "%(uploader)s | %(title)s.%(ext)s"'
+alias ytdlpl='ytdl --yes-playlist'
+alias watchlist='ytdl --yes-playlist -- "https://www.youtube.com/playlist?list=WL"'
