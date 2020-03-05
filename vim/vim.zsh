214f52b0a3a2f472e52b45dfe058e8db65c6a5e5
Casey McGinty <casey.mcginty@gmail.com>
2020-03-05 23:11:32 +0000

    Better gvim aliases

--- vim/vim.zsh
+++ vim/vim.zsh
@@ -1,7 +1,7 @@
 # Vim
 
-if [[ $(hash gvim 2> /dev/null) ]]; then
-    alias sdvi='sudo gvim -p > /dev/null 2>&1'
-    alias vi='gvim > /dev/null 2>&1'
-    alias vim='gvim > /dev/null 2>&1'
-fi
+hash gvim 2> /dev/null && VIM=gvim || VIM=vim
+
+alias sdvi="sudo $VIM -p > /dev/null 2>&1"
+alias vi="$VIM > /dev/null 2>&1"
+alias vim="$VIM > /dev/null 2>&1"
