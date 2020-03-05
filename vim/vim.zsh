3b1df31f013377df90c60bb84fbd73cc1584e7ae
Casey McGinty <casey.mcginty@gmail.com>
2020-03-05 23:06:58 +0000

    Fix test for gvim in path

--- vim/vim.zsh
+++ vim/vim.zsh
@@ -1,6 +1,6 @@
 # Vim
 
-if [[ hash gvim 2> /dev/null ]]; then
+if [[ $(hash gvim 2> /dev/null) ]]; then
     alias sdvi='sudo gvim -p > /dev/null 2>&1'
     alias vi='gvim > /dev/null 2>&1'
     alias vim='gvim > /dev/null 2>&1'
