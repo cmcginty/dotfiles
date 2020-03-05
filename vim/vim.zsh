546924f58f23f336d47e0c70a407a2d10d5e4f26
Casey McGinty <casey.mcginty@gmail.com>
2020-03-05 23:14:00 +0000

    Fix vim->vim alias bug

--- vim/vim.zsh
+++ vim/vim.zsh
@@ -4,4 +4,7 @@ hash gvim 2> /dev/null && VIM=gvim || VIM=vim
 
 alias sdvi="sudo $VIM -p > /dev/null 2>&1"
 alias vi="$VIM > /dev/null 2>&1"
-alias vim="$VIM > /dev/null 2>&1"
+
+if [[ $VIM == "gvim" ]]; then
+    alias vim="$VIM > /dev/null 2>&1"
+fi
