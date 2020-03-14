0a7aff04a2af16a244fc98747e4e49979217fbda
Casey McGinty <casey.mcginty@gmail.com>
2020-03-13 17:50:35 -0700

    Enhanced pastry commands

--- zsh/aliases-facebook.zsh
+++ zsh/aliases-facebook.zsh
@@ -1,14 +1,29 @@
 # Facebook CLI tools
 
+# Store the paste ID when creating a new paste. Then use pastry-add-cmd to
+# prepend the shell command to the paste.
+pastry-get-id() {
+    PASTE_ID=P$(
+        pastry --json |
+        jq -r "select(.type==\"data\") | .data.createdPaste.id"
+    )
+    echo "https://our.intern.facebook.com/intern/paste/${PASTE_ID}"
+}
 
 # Create a new paste that shows the actual command used to generate it:
 #   Ex:
-#       my-cmd | pastry
-#       pastry-show-cmd PXXXXX
-function pastry-show-cmd {
-   LAST_COMMAND="$(history -1 | sed -e 's/\s*[0-9]*\s*//')"
-   { echo "\$ ${LAST_COMMAND}" & pastry $1; } |
-      pastry
+#       my-cmd | paste-in
+#       pastry-add-cmd
+pastry-add-cmd() {
+    local LAST_COMMAND="$(history -1 | sed -e 's/\s*[0-9]*\s*//')"
+    { echo "\$ ${LAST_COMMAND}" & pastry ${PASTE_ID}; } | pastry
+}
+
+# Create a paste of file and output command used to "copy" the file.
+# NOTE: pbcopy only works in an ondemand
+pastry-send() {
+    cat $1 | pastry-get-id >/dev/null
+    echo -n "pastry ${PASTE_ID} > $1" | tee >(pbcopy)
 }
 
 alias ab='arc build'
