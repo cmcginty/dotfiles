ce470eb3a64bc0a845e58cbe9545afd613c5d19f
Casey McGinty <casey.mcginty@gmail.com>
2020-03-13 18:09:11 -0700

    Add more FB aliases

--- zsh/aliases-facebook.zsh
+++ zsh/aliases-facebook.zsh
@@ -9,22 +9,25 @@ pastry-get-id() {
     )
     echo "https://our.intern.facebook.com/intern/paste/${PASTE_ID}"
 }
+alias pgid='pastry-get-id'
 
 # Create a new paste that shows the actual command used to generate it:
 #   Ex:
-#       my-cmd | paste-in
+#       my-cmd | pastry-get-id
 #       pastry-add-cmd
 pastry-add-cmd() {
     local LAST_COMMAND="$(history -1 | sed -e 's/\s*[0-9]*\s*//')"
-    { echo "\$ ${LAST_COMMAND}" & pastry ${PASTE_ID}; } | pastry
+    { echo "\$ ${LAST_COMMAND}" && pastry ${PASTE_ID}; } | pastry
 }
+alias pcmd='pastry-add-cmd'
 
 # Create a paste of file and output command used to "copy" the file.
-# NOTE: pbcopy only works in an ondemand
+#   NOTE: pbcopy only works in an ondemand
 pastry-send() {
     cat $1 | pastry-get-id >/dev/null
     echo -n "pastry ${PASTE_ID} > $1" | tee >(pbcopy)
 }
+alias psend='pastry-send'
 
 alias ab='arc build'
 alias arb='arc rebuild --disable-smart'
