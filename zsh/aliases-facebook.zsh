ca088868db924b9fba341c29b75d90880fc3355c
Casey McGinty <casey.mcginty@gmail.com>
2020-05-14 18:53:16 -1000

    Update aliases

--- zsh/aliases-facebook.zsh
+++ zsh/aliases-facebook.zsh
@@ -2,7 +2,7 @@
 
 # Store the paste ID when creating a new paste. Then use pastry-add-cmd to
 # prepend the shell command to the paste.
-pastry-get-id() {
+function pastry-get-id() {
     PASTE_ID=P$(
         pastry --json |
         jq -r "select(.type==\"data\") | .data.createdPaste.id"
@@ -15,7 +15,7 @@ alias pgid='pastry-get-id'
 #   Ex:
 #       my-cmd | pastry-get-id
 #       pastry-add-cmd
-pastry-add-cmd() {
+function pastry-add-cmd() {
     local LAST_COMMAND="$(history -1 | sed -e 's/\s*[0-9]*\s*//')"
     { echo "\$ ${LAST_COMMAND}" && pastry ${PASTE_ID}; } | pastry
 }
@@ -23,7 +23,7 @@ alias pcmd='pastry-add-cmd'
 
 # Create a paste of file and output command used to "copy" the file.
 #   NOTE: pbcopy only works in an ondemand
-pastry-send() {
+function pastry-send() {
     cat $1 | pastry-get-id >/dev/null
     echo -n "pastry ${PASTE_ID} > $1" | tee >(pbcopy)
 }
@@ -45,7 +45,7 @@ alias arb='arc rebuild --disable-smart'
 alias au='arc unit'
 
 alias pm='pingme'
-alias fin='pingme "$HOSTNAME READY"'
+alias fin="pingme \"\$(hostname | rg -o '^\w+\.\w+') READY\""
 
 # Mercurial/Phabricator
 alias h='hg'
@@ -58,3 +58,7 @@ alias fixvscode='sudo systemctl restart vscode-daemon && kill $(lsof -t -i :9092
 # Certificates
 alias renew_certs='openssl x509 -checkend $((3600*4)) -noout -in /var/facebook/credentials/cmcginty/presto/cmcginty.pem || cc-certs -duo_pass push'
 alias renew_dev='expect -c "spawn renew_creds devvm3470.ftw3.facebook.com; expect Passcode\ or\ option\ (1-1):; send push\r; expect eof"'
+
+# Everstore
+alias esget='clowder get'
+function espatch() { (clowder get $1 - 2>/dev/null && echo "") | patch -p1 }
