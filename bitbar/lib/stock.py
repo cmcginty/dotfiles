ed60f250355a4daacec11f3e71c7497d3f820024
Casey McGinty <casey.mcginty@gmail.com>
2020-06-05 10:43:02 -1000

    Fix bitbar config

--- bitbar/lib/stock.py
+++ bitbar/lib/stock.py
@@ -1,22 +1,27 @@
 # -*- coding: utf8 -*-
 
-import json
 from collections import namedtuple
-from urllib import request
+import yfinance as yf
 
 Stock = namedtuple("Stock", ["ticker", "price", "percent", "source"])
 
 
 def get_quote(ticker):
-    r = request.urlopen("https://api.iextrading.com/1.0/stock/{}/quote".format(ticker))
-    data = json.loads(r.read())
-    return Stock(
-        ticker, data["latestPrice"], data["changePercent"], data["latestSource"]
-    )
+    q = yf.Ticker(ticker)
+    price = q.info["bid"]
+    open_ = q.info["open"]
+    pcnt = (price - open_) / open_
+    return Stock(ticker, price, pcnt, "close")
 
 
 def print_quote(q, show_percent=False, link=False):
-    color = "#606060" if q.source.lower() == 'close' else "red" if q.percent < 0 else "green"
+    color = (
+        "#606060"
+        if q.source.lower() == "close"
+        else "red"
+        if q.percent < 0
+        else "green"
+    )
     # direction = "⬇" if q.percent < 0 else "⬆"
     direction = ""
     output = f"{q.ticker} {q.price:0.2f}{direction}"
