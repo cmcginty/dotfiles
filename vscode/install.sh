38acd3aaea71f4bbdba0960fcde62501528f2197
Casey McGinty <casey.mcginty@gmail.com>
2020-07-24 13:50:33 -0700

    Create basic ondemand dotfiles

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,20 +7,26 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [[ "$OSTYPE" == darwin* ]]; then
 
    # Skip installing public VS code if FB version exists.
-   if hash code-fb; then
+   if hash code-fb 2>/dev/null; then
       # Install all missing packages from package-list.txt created by:
       #    gen-package-list.sh
-      cat $BASEDIR/package-list.txt | xargs -n 1 code-fb --force --install-extension || true
+      # cat $BASEDIR/package-list.txt | xargs -n 1 code-fb --force --install-extension || true
+
+      HOMEDIR=~/.ondemand/homedir/
+      mkdir -p $HOMEDIR
+      ln -fs $HOME/.hgrc $HOMEDIR
+      ln -fs $HOME/.gitconfig $HOMEDIR
+      ln -fs $HOME/.dotfiles/zsh/aliases-facebook.zsh $HOMEDIR/.aliases
    else
       # Try to install vscode ... No guarantee this will work.
-      hash code || $( \
+      hash code 2>/dev/null || $( \
          wget 'https://go.microsoft.com/fwlink/?LinkID=620882' -O /tmp/vscode.zip; \
          open /tmp/vscode.zip \
       )
    fi
 
    # Also update public VS Code if it was manually installed.
-   if hash code; then
+   if hash code 2>/dev/null; then
       # Install all missing packages from package-list.txt created by:
       #    code --list-extensions > package-list.txt
       cat $BASEDIR/package-list.txt | xargs -n 1 code --force --install-extension
