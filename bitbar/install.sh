ed5a9b2a83ad15ac6d49d42167ba39778794f959
Casey McGinty <casey.mcginty@gmail.com>
2022-07-30 14:33:58 -0700

    Remove bitbar

--- bitbar/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == darwin* ]]; then
-  test -d /Applications/BitBar.app || brew cask install bitbar
-  pip3 install --user --quiet --upgrade yfinance six lxml
-fi
--- bitbar/plugins/box.5m.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3
-
-import lib.stock as stocks
-
-q = stocks.get_quote("BOX")
-
-stocks.print_quote(q)
-print("---")
-stocks.print_quote(q, show_percent=True, link=True)
