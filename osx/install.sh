433dc3b417905c08f9128d3886e1d5629dcfa104
Casey McGinty <casey.mcginty@gmail.com>
2022-08-02 11:26:48 -0700

    Add Smooze config defaults

--- osx/install.sh
+++ osx/install.sh
@@ -5,20 +5,5 @@ if [[ "$OSTYPE" != darwin* ]]; then
     exit
 fi
 
-# 'defaults find NSUserKeyEquivalents' shows current settings.
-# Disable a shortcut by assigning the menu item to something like \U200B (ZERO-WIDTH SPACE)
-
-defaults write org.videolan.vlc NSUserKeyEquivalents '{
-    "Step Backward" = "\U2190";
-    "Step Forward" = "\U2192";
-}'
-
-defaults write com.google.Chrome NSUserKeyEquivalents '{
-    "Close Tab" = "^w";
-    "Find..." = "^f";
-    "New Tab" = "^t";
-    "Reload This Page" = "^r";
-    "Reopen Closed Tab" = "^$t";
-    "Select Next Tab" = "^l";
-    "Select Previous Tab" = "^h";
-}'
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+for config_file in $BASEDIR/*.cfg; do source "$config_file"; done
