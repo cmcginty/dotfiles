4eb5ff5042dcbae2930f40833981086b247c2ece
Casey McGinty <casey.mcginty@gmail.com>
2024-11-24 22:16:06 -0800

    Add linearmouse

--- tools/homebrew.sh
+++ tools/homebrew.sh
@@ -7,4 +7,5 @@ if [[ "$OSTYPE" == darwin* ]]; then
   test -d /Applications/Rectangle.app || brew install rectangle --cask
   test -d /Applications/Stats.app || brew install stats --cask
   test -d "/Applications/Silicon Info.app" || brew install silicon-info --cask
+  test -d /Applications/LinearMouse.app || brew install linearmouse --cask
 fi
